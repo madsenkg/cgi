16591c68530e03baaf6d2a52be5ceb37c00f6134
Kenneth Madsen <mydevops@outlook.dk>
2022-11-04 07:26:25 +0100

    Updated Install file

--- install.ps1
+++ install.ps1
@@ -1,15 +1,16 @@
-cls
+Clear-Host
+
+$Script:ScriptDir = Split-Path -Parent $PSCommandPath;
+Set-Location $Script:ScriptDir;
+
 $documentFolder = [Environment]::GetFolderPath("MyDocuments")
 #$downloadFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 $scriptFolder = join-path  $documentFolder "scripts"
 
 #Check if Script folder is in place
-
-
-#"Test to see if folder [$Folder]  exists"
 if (Test-Path -Path $scriptFolder) {
     "Path exists!"
-    Remove-Item -Path $scriptFolder -Filter *.ps1 -Force -Verbose
+    Remove-Item $scriptFolder -Include *.ps1 -Force -Verbose
 } else {
     "Path doesn't exist."
     New-Item -Path $scriptFolder -ItemType "Directory" -Force -Verbose
@@ -18,8 +19,10 @@ if (Test-Path -Path $scriptFolder) {
 #Download the Zip files
 
 #Unzip the The DevAdmin Files and copy the files to Script folder
-Expand-Archive .\vmdevadmin-main.zip -DestinationPath . -Force -Verbose
-Copy-Item .\vmdevadmin-main -Destination $scriptFolder -Force -Verbose
+Get-ChildItem -Path .\
+Expand-Archive .\vmdevadmin-main.zip -DestinationPath .\ -Force -Verbose
+Copy-Item .\vmdevadmin-main\*.* -Destination $scriptFolder -Force -Verbose
+Remove-Item .\vmdevadmin-main -Force -Recurse -Verbose
 
 
 #Unzip the vm setup file to the downlod folder

640d53766d7ae297514d37b097ceb9a1bf8b6ec0
Kenneth Madsen <mydevops@outlook.dk>
2022-11-04 09:32:51 +0100

    testing choco

---
+++ install_choco.ps1
@@ -0,0 +1,11 @@
+Set-LocalUser -SID (([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value) -PasswordNeverExpires $true
+Invoke-Expression (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
+$env:Path + ';%ALLUSERSPROFILE%\chocolatey\bin'
+choco install googlechrome -y -v
+choco install microsoft-edge -y -v
+choco install microsoftazurestorageexplorer -y -v
+choco install sql-server-management-studio -y -v
+choco install vscode -y -v
+choco install notepadplusplus -y -v 
+choco install windirstat -y -v
+choco install postman -y -v

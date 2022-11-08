5e788218fa1de425bd07747dc20d7f67c841ba79
madsenkg <102599096+madsenkg@users.noreply.github.com>
2022-11-08 14:36:12 +0100

    Update install-choco.ps1
    adding transcripts logging

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,6 +1,10 @@
 #Requires -RunAsAdministrator
+#Start log
+Start-Transcript -Append -Path (join-path $env:TEMP ("cgiscripts\{0}_{1}.log" -f $env:COMPUTERNAME,(Get-Date -format yyyyMMdd))) 
+
 Invoke-Expression (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1') -WarningAction SilentlyContinue
 $env:Path += ';%ALLUSERSPROFILE%\chocolatey\bin'
+
 choco install googlechrome -y -v
 choco install microsoft-edge -y -v
 choco install microsoftazurestorageexplorer -y -v
@@ -9,3 +13,5 @@ choco install vscode -y -v
 choco install notepadplusplus -y -v 
 choco install windirstat -y -v
 choco install postman -y -v
+
+Stop-Transcript
